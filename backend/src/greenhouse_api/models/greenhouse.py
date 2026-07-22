c649400b7b6afd52bff17a7ecb4f23a269da7236
Anthony Huggins <anthonyhugginsdev@gmail.com>
2026-07-22 17:08:48 -0400

    add GreenhouseCreate class for creating new greenhouse object and validating input with pydantic

--- backend/src/greenhouse_api/models/greenhouse.py
+++ backend/src/greenhouse_api/models/greenhouse.py
@@ -26,5 +26,21 @@ class Greenhouse(BaseModel):
     created_at: datetime = Field(description="When this greenhouse was created")
     updated_at: datetime = Field(description="When this greenhouse was last updated")
 
+class GreenhouseCreate(BaseModel):
+    """Pydantic class representing a single greenhouse, used for creating a new greenhouse
+    
+    Must have ONLY the follwing fields:
+        name: str - The name of the greenhouse
+        num_rows: int - how many rows for locations this greenhouse has (MAX - 6)
+        locations_per_row: int - How many pots(locations) can fit in each row in this greenouse (MAX - 11)
+
+    """
+
+    model_config = ConfigDict(extra="forbid")
+
+    name: str = Field(max_length=25, description="Name of the greenhouse")
+    num_rows: int = Field(gt=0, lt=6, description="how many rows are in the greenhouse")
+    locations_per_row: int = Field(gt=0, lt=11, description="How many pots(locations) can fit in each row")
+    
 
   
